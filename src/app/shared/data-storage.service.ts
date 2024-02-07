1ef81e85e7b565fc5c204951d6e4b001c983f9de
lerman251 <63618979+lerman251@users.noreply.github.com>
2024-02-07 14:40:51 -0600

    first commit

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+
+@Injectable({ providedIn: 'root' })
+
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipeService: RecipeService ) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://ng-access-backend-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response => {
+            console.log(response);
+        });
+    }
+    fetchRecipes() {
+        this.http.get<Recipe[]>('https://ng-access-backend-default-rtdb.firebaseio.com/recipes.json').subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
+        });
+    }
+}
